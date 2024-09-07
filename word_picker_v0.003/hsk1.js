b9e2dd1d550c45cfe65d7b176996800136cc2b40
eamork <100851108+eamork@users.noreply.github.com>
2024-09-07 10:42:25 +0200

    Add files via upload

---
+++ word_picker_v0.003/hsk1.js
@@ -0,0 +1,14 @@
+const hsk1Data = {
+    sentenceToForm: ['我', '喜欢', '猫'],  // Chinese sentence 1 (I like cats)
+    englishSentence: 'I like cats',
+    pinyinSentence: 'Wǒ xǐhuān māo'
+  };
+  
+  const hsk1Data2 = {
+    sentenceToForm: ['你', '在哪儿'],  // Chinese sentence 2 (Where are you?)
+    englishSentence: 'Where are you?',
+    pinyinSentence: 'Nǐ zài nǎr'
+  };
+  
+  startGame(hsk1Data);  // You can alternate or randomize sentences later
+  
---
+++ word_picker_v0.003/hsk4.js
@@ -0,0 +1,14 @@
+const hsk4Data = {
+    sentenceToForm: ['他', '工作', '非常', '努力'],  // Chinese sentence 1 (He works very hard)
+    englishSentence: 'He works very hard',
+    pinyinSentence: 'Tā gōngzuò fēicháng nǔlì'
+  };
+  
+  const hsk4Data2 = {
+    sentenceToForm: ['这', '个', '问题', '很', '复杂'],  // Chinese sentence 2 (This problem is very complicated)
+    englishSentence: 'This problem is very complicated',
+    pinyinSentence: 'Zhège wèntí hěn fùzá'
+  };
+  
+  startGame(hsk4Data);
+  
